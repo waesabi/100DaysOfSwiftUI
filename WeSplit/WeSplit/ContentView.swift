2c4a9623a0b34ca519ce23022d7f2a83d43b080f
Sanket Kumar <sanketkumar@microsoft.com>
2022-07-16 16:29:21 +0530

    WeSplit init

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Sanket Kumar on 16/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sanket Kumar on 16/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
