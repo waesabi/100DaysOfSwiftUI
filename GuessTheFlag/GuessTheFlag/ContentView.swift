90f334747dfc9fbb36dda70b3bca4ea1202a7754
Sanket Kumar <sanketkumar@microsoft.com>
2022-07-17 10:46:01 +0530

    GuessTheFlag - Showing alert messages 🚀

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,32 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+	
+	@State private var showAlert = false
+	
     var body: some View {
-		VStack(spacing: 16) {
-			Button("Button 1") { }
-				.buttonStyle(.bordered)
-			
-			Button("Button 2", role: .destructive) { }
-				.buttonStyle(.bordered)
-			
-			Button("Button 3") { }
-				.buttonStyle(.borderedProminent)
-				.tint(.mint)
-			
-			Button("Button 4", role: .destructive) { }
-				.buttonStyle(.borderedProminent)
-			
-			Button {
-				print("Edit is tapped")
-			} label: {
-				Image(systemName: "pencil")
-			}
-			
-			Button {
-				print("Edit is tapped")
-			} label: {
-				Label("Edit", systemImage: "pencil")
-			}
+		Button("Show Alert") {
+			showAlert = true
+		}
+		.alert("Important message", isPresented: $showAlert) {
+			Button("Delete", role: .destructive) { }
+			Button("Cancel", role: .cancel) { }
+		} message: {
+			Text("Please read this")
 		}
     }
 	
