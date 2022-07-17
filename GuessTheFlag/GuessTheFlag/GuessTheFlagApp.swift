d82164293a98d1aa6b3db3fe6e883f6166d539b6
Sanket Kumar <sanketkumar@microsoft.com>
2022-07-17 10:10:52 +0530

    GessTheFlag Created

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sanket Kumar on 17/07/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
